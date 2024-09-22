e469c96464b77fad91d53d317a5bb6a50d9b4ead
mamta1998gola <mamta1998gola@gmail.com>
2024-09-22 16:14:41 +0530

    reduced the padding by half

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import QRCodeGenerator from '@/components/QRCodeGenerator'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <QRCodeGenerator />
     </main>
   )
