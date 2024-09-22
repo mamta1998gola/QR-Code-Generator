5c16f8ccbe231a7540d555b8af830431c14cfd78
mamta1998gola <mamta1998gola@gmail.com>
2024-09-22 15:45:27 +0530

    updated input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,8 +1,5 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
-
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> { }
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
 const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
@@ -16,10 +13,9 @@ const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLI
         ref={ref}
         {...props}
       />
-    )
+    );
   }
 );
+Input.displayName = "Input";
 
-Input.displayName = "Input"
-
-export { Input }
+export { Input };
